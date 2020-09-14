4c2a11cff03e4705af0b55f8a13c6b9a8764fc49
Vinoth Manoharan <vinothm.manoharan@gamil.com>
2020-09-14 14:22:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.Vinoth5595/arithmetic/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.github.Vinoth5595/arithmetic)
+
+# arithmetic
+Example library for tutorial on publishing to Maven Central with OSSRH
---
+++ src/main/java/com/github/vinoth5595/arithmetic/Operation.java
@@ -0,0 +1,21 @@
+package com.github.vinoth5595.arithmetic;
+
+import java.math.BigDecimal;
+
+public class Operation {
+	public static BigDecimal add(BigDecimal x, BigDecimal y) {
+		return x.add(y);
+	}
+
+	public static BigDecimal substract(BigDecimal x, BigDecimal y) {
+		return x.subtract(y);
+	}
+
+	public static BigDecimal multiply(BigDecimal x, BigDecimal y) {
+		return x.multiply(y);
+	}
+
+	public static BigDecimal divide(BigDecimal x, BigDecimal y) {
+		return x.divide(y);
+	}
+}
